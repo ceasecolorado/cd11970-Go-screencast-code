453184299322fe92260da2beff54652cf4c1445d
coloradc <coloradc@mskcc.org>
2023-05-07 07:55:41 -0400

    learned functions

---
+++ L2. Go Fundamentals I/3. Conditionals/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	language := "Go"
+
+	if language == "Go" {
+		fmt.Println("You are learning Go! Great choice!")
+	} else {
+		fmt.Println("So you are not learning Go. What are you learning?")
+	}
+}
